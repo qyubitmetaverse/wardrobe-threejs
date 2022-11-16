b7cf37f6cd1738835468caec12f0ecc2a373cd99
Apuy717 <abdulgopur2306@gmail.com>
2022-11-16 14:15:55 +0700

    warning mobile

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -25,7 +25,7 @@ const Home: NextPage = () => {
           viewBox="0 0 24 24"
           strokeWidth={1.5}
           stroke="currentColor"
-          className="w-24 h-24 rotate-90"
+          className="w-24 h-24 rotate text-red-600"
         >
           <path
             strokeLinecap="round"
@@ -33,7 +33,10 @@ const Home: NextPage = () => {
             d="M10.5 1.5H8.25A2.25 2.25 0 006 3.75v16.5a2.25 2.25 0 002.25 2.25h7.5A2.25 2.25 0 0018 20.25V3.75a2.25 2.25 0 00-2.25-2.25H13.5m-3 0V3h3V1.5m-3 0h3m-3 18.75h3"
           />
         </svg>
-        <p>Pleace change to landscape your device</p>
+        <div className="text-center py-2">
+          <p>Anda berada dalam Mode Depelopment!!</p>
+          <p>Sementara untuk perangkat mobile belum tersedia</p>
+        </div>
       </div>
 
       <button
